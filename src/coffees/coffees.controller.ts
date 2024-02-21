69e7028c13f73f7286c7c3b18091fab4401256d5
Md.Asif Parvez Sarker <asif.sarker@selise.ch>
2024-02-21 10:26:19 +0600

    Exception handling

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,32 +1,38 @@
-import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Patch, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpCode, HttpStatus, Patch, Delete, Query, Param, HttpException, NotFoundException } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import {Coffee} from './entities/coffee.entity'
 
 @Controller('coffees')
 export class CoffeesController {
 
+    constructor(private readonly coffeesService: CoffeesService){}
     
     @Get()
-    findAll(@Query() paginationQuery): string {
-        const {offset, limit} = paginationQuery;
-        return `the offset is ${offset} and limit is ${limit}`
+    findAll(@Query() paginationQuery): Coffee[] {
+        // const {offset, limit} = paginationQuery;
+        throw new NotFoundException()
+        return this.coffeesService.findAll()
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string): string {
-        return `The Param is ${id}`
+    findOne(@Param('id') id: string): Coffee {
+        return this.coffeesService.findOne(id)
     }
 
     
    
     @Post()
     @HttpCode(HttpStatus.CREATED)
-    create(@Body('name') body){
-        return body
+    create(@Body() body){
+        return this.coffeesService.create(body)
     }
 
     @Patch(":id")
     update(@Param('id') id: string, @Body() body){
         return `Successfully body updated with id: ${id} and body data name ${body.name}`
     }
+
+    
 
     @Delete(':id')
     remove(@Param('id') id: string){
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+    private coffee: Coffee[] = [
+        {
+            id: 1,
+            name: "Best Coffee",
+            brand: "best",
+            flavors: ['cho', 'slf']
+        }
+    ]
+
+    findAll(): Coffee[]{
+        return this.coffee
+    }
+
+    findOne(id): Coffee {
+        console.log(typeof id)
+        console.log(typeof +id)
+        return this.coffee.find(item => item.id === +id)
+    }
+
+    create(createCoffeeDto: Coffee){
+        return this.coffee.push(createCoffeeDto)
+    }
+
+    update(id: string, updateCoffeeDto: Coffee){
+        const existingcoffee = this.findOne(id)
+        if(existingcoffee){
+            return `Existing Coffee Updated` 
+        }
+    }
+
+    remove(id: string){
+        const removeItemIndex = this.coffee.findIndex(item => item.id === +id)
+        if(removeItemIndex !== 1){
+            this.coffee.splice(removeItemIndex,1)
+        }
+    }
+
+}
